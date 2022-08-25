ea663a7adcba08ae5e9ff3690ffe3048a1455295
Karl Palsson <karlp@etactica.com>
2022-08-25 09:21:15 +0000

    setup.py: fix project URL
    Signed-off-by: Karl Palsson <karlp@etactica.com>

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A Bluetooth HCI traffic snooping and parsing module.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/traviswpeters/btsnoop",
     packages=setuptools.find_packages(),
     classifiers=[
         "Bluetooth :: Python :: 3",
